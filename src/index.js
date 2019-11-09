aec92e106645568c504cc9047cceadb883c2c999
loockeeer <lulu.r1313@gmail.com>
2019-11-09 11:09:30 +0100

    Removed client propertie init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const { AkairoClient } = require('discord-akairo');
 const config = require('./config.json');
-
 const client = new AkairoClient({
     ownerID: config.OWNERS,
-    prefix: require('./token.json'),
+    prefix: config.PREFIX,
     commandDirectory: './commands',
     listenerDirectory: './events'
 });
+
 client.config = config
-client.updateGuild = require('./utils/updateGuild.js');
-client.login(config.TOKEN)
+client.refreshModeration = require('./utils/refreshModeration.js');
+client.login(require('./token.json'))
