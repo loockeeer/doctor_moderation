4c1dc36cf9fb86a17ca39c8af71b11f7870177ba
Loockeeer <34001773+loockeeer@users.noreply.github.com>
2019-11-11 13:14:28 +0100

    Update percent.js

--- percent.js
+++ percent.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 const README = fs.readFileSync('./README.md', 'utf8');
 const todo = README.match(/\[ \]/g);
 const coded = README.match(/\[x\]/g);
-console.log(coded.length/todo.length);
+console.log(`We have done ${Math.floor((coded.length/todo.length)*100)}% of the project !`);
