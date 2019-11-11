ccbf976b014f423149f88393a8f7c01212166b21
loockeeer <lulu.r1313@gmail.com>
2019-11-11 13:16:29 +0100

    Some changes...

--- src/events/custom-ban.js
+++ src/events/custom-ban.js
@@ -13,11 +13,11 @@ class CustomBanListener extends Listener {
     }
 
     exec(target, mod, message, reason) {
-        const humanTime = reason.match(/((\dw)|(\dd)|(\dm))/);
+        const humanTime = reason.match(/((\d\dw)|(\d\dd)|(\d\dm)|(\d\ds))/);
         let time;
 
-        if(humanTime) {
-            reason = reason.replace(humanTime, '');
+        if (humanTime) {
+            reason = reason.replace(humanTime[0], '');
             switch (humanTime[0].slice(-1)) {
                 case 'd':
                     time = 24 * 60000 * Number(humanTime[0].replace('d', '')) * 1000;
@@ -27,13 +27,16 @@ class CustomBanListener extends Listener {
                     break;
                 case 'm':
                     time = 4 * 7 * 24 * 60000 * Number(humanTime[0].replace('m', '')) * 1000;
-                    break
+                    break;
+                case 's':
+                    time = Number(humanTime[0].replace('s', '')) * 1000;
+                    break;
             }
         }
         const embed = new RichEmbed()
             .setAuthor('Ban', mod.user.displayAvatarURL)
             .setThumbnail(target.user.displayAvatarURL)
-            .addField('Modérateur', mod.user.tag,true)
+            .addField('Modérateur', mod.user.tag, true)
             .addField('Cible', target.user.tag, true)
             .addField('Raison', reason ? reason : 'Aucune', true)
             .addField('Durée', time ? humanTime[0].replace('d', ' jour(s)').replace('w', ' semaine(s)').replace('m', 'mois') : "Infinie", true)
@@ -44,22 +47,22 @@ class CustomBanListener extends Listener {
             .addField('Action', 'Ban', true)
             .addField('Modérateur', mod.user.tag, true)
             .addField('Raison', reason ? reason : 'Aucune', true)
-            .addField('Durée', time ? humanTime[0].replace('d', ' jour(s)').replace('w', ' semaine(s)').replace('m', 'mois') : "Infinie", true)
+            .addField('Durée', time ? humanTime[0].replace('d', ' jour(s)').replace('w', ' semaine(s)').replace('m', ' mois') : "Infinie", true)
             .setTimestamp()
             .setColor(this.client.config.colors.RED);
-        if(mod.user.id !== this.client.user.id) {
+        if (mod.user.id !== this.client.user.id) {
             message.channel.send(embed);
         }
-        target.createDM().then(channel=>{
-            channel.send(embed).then(()=>{
-                return target.ban({reason: reason})
-            }).catch(()=>target.ban({reason: reason}))
-        }).catch(()=>target.ban({reason: reason}));
+        //target.createDM().then(channel=>{
+        //channel.send(embed).then(()=>{
+        //return target.ban({reason: reason})
+        //}).catch(()=>target.ban({reason: reason}))
+        //}).catch(()=>target.ban({reason: reason}));
         this.client.channels.get(this.client.config.channels.MOD_LOG).send(logEmbed);
 
         conn.query(`
 mutation {
-  createBan(input: {ban: {createdAt: "${Date.now()}", targetId: "${target.user.id}" ${time ? 'time:"'+time+'", ' : ','} ${reason ? reason+"," : 'Aucune'} moderatorId: "${mod.user.id}"}}) {
+  createBan(input: {ban: {createdAt: "${Date.now()}", targetId: "${target.user.id}", moderatorId: "${mod.user.id}", time: "${time || -1}", reason: "${reason || 'Aucune'}"}}) {
     ban {
       createdAt
     }
--- src/events/custom-warn.js
+++ src/events/custom-warn.js
@@ -16,30 +16,41 @@ class CustomWarnListener extends Listener {
         const embed = new RichEmbed()
             .setAuthor('Warn', mod.user.displayAvatarURL)
             .setThumbnail(target.user.displayAvatarURL)
-            .addField('Modérateur', mod.user.tag,true)
+            .addField('Modérateur', mod.user.tag, true)
             .addField('Cible', target.user.tag, true)
             .addField('Raison', reason ? reason : 'Aucune', true)
             .setTimestamp()
-            .setColor(this.client.config.colors.RED)
+            .setColor(this.client.config.colors.RED);
         const logEmbed = new RichEmbed()
             .setAuthor(target.user.tag, target.user.displayAvatarURL)
             .addField('Action', 'Warn', true)
             .addField('Modérateur', mod.user.tag, true)
             .addField('Raison', reason ? reason : 'Aucune', true)
             .setTimestamp()
-            .setColor(this.client.config.colors.RED)
-        target.createDM().then(channel=>{
-            channel.send(embed).catch(()=>{})
-        })
-        this.client.channels.get(this.client.config.channels.MOD_LOG).send(logEmbed)
-        if(mod.user.id !== this.client.user.id) {
+            .setColor(this.client.config.colors.RED);
+        target.createDM().then(channel => {
+            channel.send(embed).catch(() => {
+            })
+        });
+        this.client.channels.get(this.client.config.channels.MOD_LOG).send(logEmbed);
+        if (mod.user.id !== this.client.user.id) {
             message.channel.send(embed)
         }
         // Request to the databasse to warn the user
-        console.log('ABADAKOR')
         db.query(`
 mutation {
-  createWarn(input: {warn: {createdAt: "${Date.now()}", moderatorId: "${mod.user.id}", targetId: "${target.user.id}", reason: "${reason || ''}"}}) {
+  createModerationAction(
+    input: {
+      moderationAction: {
+        moderatorId: "${mod.user.id}"
+        type: WARN
+        targetId: "${target.user.id}"
+        createdAt: "${Date.now()}"
+        ${reason ? `createdReason: "${reason}"` : ''}
+        ${duration ? `duration: "${duration}"` : ''}
+      }
+    }
+  ) {
     clientMutationId
   }
 }
