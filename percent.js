71c6758310c1273e21872925b021a1d6f955bc98
Loockeeer <34001773+loockeeer@users.noreply.github.com>
2019-11-11 13:11:03 +0100

    Update percent.js

--- percent.js
+++ percent.js
@@ -1,5 +1,3 @@
 const fs = require('fs');
 const README = fs.readFileSync('./README.md');
-const toDoOccurs = /\[ \]/g.match(README);
-const codedOccurs = /\[x\]/g.match(README);
-console.log(codedOccurs.length/toDoOccurs.length);
+console.log(README);
